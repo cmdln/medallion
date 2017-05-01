f1db24606b46a43a87b0f94b7d66b41f7c2c2ee3
Thomas Gideon <cmdln@thecommandline.net>
2017-05-01 13:38:04 -0400

    Upgrade to Serde 1.0 (#2)
    * Bump openssl
    * Fix naive code that was breaking serde 1.0

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,8 @@ extern crate serde_derive;
 extern crate serde_json;
 extern crate time;
 
-use serde::{Serialize, Deserialize};
+use serde::Serialize;
+use serde::de::DeserializeOwned;
 pub use error::Error;
 pub use header::Header;
 pub use header::Algorithm;
@@ -29,10 +30,7 @@ pub type DefaultToken<H> = Token<H, ()>;
 
 /// Main struct representing a JSON Web Token, composed of a header and a set of claims.
 #[derive(Debug, Default)]
-pub struct Token<H, C>
-    where H: Serialize + Deserialize + PartialEq,
-          C: Serialize + Deserialize + PartialEq
-{
+pub struct Token<H, C> {
     raw: Option<String>,
     pub header: Header<H>,
     pub payload: Payload<C>,
@@ -40,8 +38,8 @@ pub struct Token<H, C>
 
 /// Provide the ability to parse a token, verify it and sign/serialize it.
 impl<H, C> Token<H, C>
-    where H: Serialize + Deserialize + PartialEq,
-          C: Serialize + Deserialize + PartialEq
+    where H: Serialize + DeserializeOwned,
+          C: Serialize + DeserializeOwned
 {
     pub fn new(header: Header<H>, payload: Payload<C>) -> Token<H, C> {
         Token {
@@ -89,8 +87,8 @@ impl<H, C> Token<H, C>
 }
 
 impl<H, C> PartialEq for Token<H, C>
-    where H: Serialize + Deserialize + PartialEq,
-          C: Serialize + Deserialize + PartialEq
+    where H: PartialEq,
+          C: PartialEq
 {
     fn eq(&self, other: &Token<H, C>) -> bool {
         self.header == other.header && self.payload == other.payload
